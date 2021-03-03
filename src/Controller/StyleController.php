3cb4c5de418394bf14268bdb958042ef6995f73a
ron2cuba <ronan.lenouvel@gmail.com>
2021-03-03 11:39:12 +0100

    feat: route create

--- src/Controller/StyleController.php
+++ src/Controller/StyleController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\StyleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StyleController extends AbstractController
 {
@@ -15,6 +16,27 @@ class StyleController extends AbstractController
     {
         return $this->render('style/index.html.twig', [
             'controller_name' => 'StyleController',
+        ]);
+    }
+    /**
+     * @Route("/admin/style/create", name="style_creatStyle")
+     */
+    public function createStyle(): Response
+    {
+        return $this->render('admin/create/create-style.html.twig');
+    }
+
+    /**
+     * @Route("/admin/edit/styles-list", name="style_editStylesList") 
+     */
+    public function stylesToEdit(StyleRepository $styleRepository): Response
+    {
+        $page = 'Edit Styles';
+        $styles = $styleRepository->findAll();
+
+        return $this->render('admin/edit/styles-list.html.twig', [
+            'styles' => $styles,
+            'page'=>$page,
         ]);
     }
 }
