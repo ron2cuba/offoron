d55062d1db56d91f52323ac07bd8bb2098ffe038
ron2cuba <ronan.lenouvel@gmail.com>
2021-03-10 15:04:06 +0100

    feat: form styles cretae and modify

--- src/Controller/StyleController.php
+++ src/Controller/StyleController.php
@@ -2,41 +2,93 @@
 
 namespace App\Controller;
 
+use App\Form\StyleType;
+use App\Repository\BandRepository;
 use App\Repository\StyleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StyleController extends AbstractController
 {
     /**
-     * @Route("/style", name="style")
+     * Display all Styles in Db
+     * @Route("/styles", name="style")
      */
-    public function index(): Response
+    public function view(StyleRepository $styleRepository): Response
     {
-        return $this->render('style/index.html.twig', [
-            'controller_name' => 'StyleController',
+        echo('yeah');
+        /* $styles =$styleRepository->findAll();
+        return $this->render('styles-list/view.html.twig', [
+            'styles' => $styles,
+        ]); */
+    }
+    /**
+     * Display style page edition
+     * @Route("admin/edit/style/{id}", name="style_editStyle")
+     */
+    public function editStyle($id, StyleRepository $styleRepository, Request $request, EntityManagerInterface $em, SluggerInterface $slugger)
+    {
+        $style = $styleRepository->find($id);
+
+        $form = $this->createForm(StyleType::class, $style);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $em->flush();
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('admin/edit/edit-style.html.twig', [
+            'style'=>$style,
+            'formView'=>$formView
+
         ]);
     }
     /**
+     * Display style page creation
      * @Route("/admin/style/create", name="style_creatStyle")
      */
-    public function createStyle(): Response
+    public function createStyle(Request $request, StyleRepository $styleRepository, SluggerInterface $slugger, EntityManagerInterface $em): Response
     {
-        return $this->render('admin/create/create-style.html.twig');
+        $form = $this->createForm(StyleType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $band = $form->getData();
+            // $style->setSlug(\strtolower($slugger->slug($style->getName())));
+            $em->persist($band);
+            $em->flush();
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('admin/create/create-style.html.twig', [
+            'formView'=>$formView
+        ]);
     }
 
     /**
+     * Display the list of bands that can be edited
      * @Route("/admin/edit/styles-list", name="style_editStylesList") 
      */
     public function stylesToEdit(StyleRepository $styleRepository): Response
     {
-        $page = 'Edit Styles';
         $styles = $styleRepository->findAll();
+        $page = "Edition des styles :";
 
         return $this->render('admin/edit/styles-list.html.twig', [
             'styles' => $styles,
-            'page'=>$page,
+            'page'=> $page,
         ]);
     }
+
+
 }
